75bf17629964f30d5e4f759b36783f3a5a31b42b
Sam Liu <sam@Sams-MacBook-Air.local>
2021-09-11 19:33:29 -0400

    Combined previous project, fixed flux tube

---
+++ Assets/Scripts/ChangeOrbit.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+/// <summary>
+/// This class implements rotation of quark along it's orbit and transitions between random orbits.
+/// </summary>
+public class ChangeOrbit : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Maximum allowed orbit radius.")]
+    private float maxOrbitRadius = 20f;
+
+    [SerializeField]
+    [Tooltip("Minimal allowed orbit radius.")]
+    private float minOrbitRadius = 7.0f;
+
+    [SerializeField]
+    [Tooltip("Orbital velocity.")]
+    private float orbitalVelocity = 300f;
+
+    [SerializeField]
+    [Tooltip("Time for quark to stay on the same orbit.")]
+    private float timeOnTheSameOrbit = 10f;
+
+    [SerializeField]
+    [Tooltip("Velocity for orbit transition.")]
+    private float orbitChangeVelocity = 100.0f;
+
+    private bool isPolarized = false;
+
+    private bool isSmallOrbits = false;
+
+    private float currentRadius = 10.0f;
+
+    private float targetRadius;
+
+    private Tween tween;
+
+    /// <summary>
+    /// Current speed of the quark.
+    /// </summary>
+    public float EstimatedSpeed { get; private set;} = 0.0f;
+
+    /// <summary>
+    /// Current orbit radius of the quark.
+    /// </summary>
+    public float CurrentOrbitRadius { get; private set; }
+
+    /// <summary>
+    /// Maximum possible radius for the quark.
+    /// </summary>
+    public float MaxRadius { get { return maxOrbitRadius; } }
+
+    /// <summary>
+    /// Is quark polarized right now.
+    /// </summary>
+    public bool IsPolarized { get { return isPolarized; } }
+
+    /// <summary>
+    /// Is quark in small orbits mode.
+    /// </summary>
+    public bool IsSmallOrbits { get { return isSmallOrbits; } }
+
+    /// <summary>
+    /// Enable or disable small orbits mode for this quark.
+    /// </summary>
+    public void SetSmallRadius(bool isSmall)
+    {
+        if (isSmallOrbits == isSmall)
+        {
+            return;
+        }
+
+        StopAllCoroutines();
+
+        maxOrbitRadius = isSmall ? (maxOrbitRadius / 10.0f) : (maxOrbitRadius * 10.0f);
+        isSmallOrbits = isSmall;
+
+        StartCoroutine(ChangeOrbitWithDelay(orbitChangeVelocity * 3.0f));
+    }
+
+    /// <summary>
+    /// Enable or polarization small orbits mode for this quark.
+    /// </summary>
+    public void SetPolarization(bool polarize)
+    {
+        if (polarize == isPolarized)
+        {
+            return;
+        }
+
+        StopAllCoroutines();
+
+        transform.position = polarize ? new Vector3(transform.position.x, transform.position.y, 0.0f) : new Vector3(transform.position.x, 0.0f, transform.position.z);
+        isPolarized = polarize;
+
+        StartCoroutine(ChangeOrbitWithDelay(orbitChangeVelocity * 3.0f));
+    }
+
+    private void Start()
+    {
+        //StartCoroutine(ChangeOrbitWithDelay(orbitChangeVelocity));
+
+        // Enable polarization
+        isPolarized = true;
+        transform.position =  new Vector3(transform.position.x, transform.position.y, 0.0f);
+
+        // Disable small orbits mode
+        isSmallOrbits = false;
+        StartCoroutine(ChangeOrbitWithDelay(orbitChangeVelocity * 3.0f));
+    }
+
+    private IEnumerator ChangeOrbitWithDelay(float changeVelocity)
+    {
+        while (true)
+        {
+            // Get new radius value
+            targetRadius = RandomGaussian.Range(minOrbitRadius, maxOrbitRadius);
+            // Calculate transition time based on velocity
+            float difference = Mathf.Abs(transform.position.magnitude - targetRadius);
+            float t = difference / changeVelocity;
+
+            // Initiate new orbit transition
+            tween?.Kill();
+            // Smoothly blend current radius to a new value
+            tween = DOTween.To(() => currentRadius, v => currentRadius = v, targetRadius, t);
+            tween.SetEase(Ease.InOutQuad);
+
+            yield return new WaitForSeconds(timeOnTheSameOrbit);
+        }
+    }
+
+    private void Update()
+    {
+        Vector3 pos = transform.position;
+        Vector3 rotationVector = isPolarized ? Vector3.forward : Vector3.up;
+
+        // First rotate current position along rotationVector with specified orbital speed
+        // float speed = orbitalVelocity * QuarkSettings.Instance.GlobalSpeedMultiplier;
+        float speed = orbitalVelocity;
+        Vector3 rotated = Quaternion.AngleAxis(speed / pos.magnitude * Time.deltaTime, rotationVector) * pos;
+
+        // Then calculate new position based on direction from old rotation position and orbit radius
+        Vector3 newPosition = rotated.normalized * currentRadius;
+
+        // Estimate current seed and orbit radius for public getters
+        EstimatedSpeed = Vector3.Distance(newPosition, transform.position) / Time.deltaTime;
+        CurrentOrbitRadius = newPosition.magnitude;
+
+        transform.position = newPosition;
+    }
+}
---
+++ Assets/Scripts/Quark.cs
@@ -0,0 +1,153 @@
+﻿using DG.Tweening;
+using System;
+using UnityEngine;
+
+public enum QuarkColor
+{
+    Red,
+    Green,
+    Blue,
+    GluonRed,
+    GluonGreen,
+    GluonBlue,
+    AntiRed,
+    AntiGreen,
+    AntiBlue
+}
+
+/// <summary>
+/// Set color and flavor of quark.
+/// </summary>
+public class Quark : MonoBehaviour
+{
+    /// <summary>
+    /// Color of sphere around quark.
+    /// </summary>
+
+    [SerializeField]
+    [Tooltip("Color of sphere around quark")]
+    private QuarkColor quarkColor;
+
+    [SerializeField]
+    private GameObject quarkSphere;
+
+    [SerializeField]
+    private GameObject quarkCore;
+
+    [SerializeField]
+    private GameObject quarkGlow;
+
+    private TrailRenderer trailRenderer;
+
+    public Material sphereMaterial;
+
+    public Material coreMaterial;
+
+    public Material glowMaterial;
+
+    public float Radius = 0.5f;
+
+    private ChangeOrbit changeOrbit;
+
+    public float trailWidth = 3.5f;
+
+    public float trailTime = 0.8f;
+
+    [SerializeField]
+    private AnimationCurve trailSizeCorrectionCurve;
+
+    /// <summary>
+    /// Set color to quark.
+    /// </summary>
+    /// <param name="newColor"></param>
+    public void SetColor(QuarkColor newColor)
+    {
+        quarkColor = newColor;
+        UpdateColor();
+    }
+
+    public float GetEstimatedSpeed()
+    {
+        return changeOrbit.EstimatedSpeed;
+    }
+
+    private void UpdateColor()
+    {
+        sphereMaterial.SetColor("_Color", QuarkSettings.GetColorRGB(quarkColor));
+        coreMaterial.SetColor("_Color", QuarkSettings.GetColorRGB(quarkColor));
+        glowMaterial.SetColor("_Color", QuarkSettings.GetColorRGB(quarkColor));
+        trailRenderer.material.SetColor("_Color", QuarkSettings.GetColorRGB(quarkColor));
+    }
+
+    /// <summary>
+    /// Get quark's color.
+    /// </summary>
+    /// <returns></returns>
+    public QuarkColor GetColor()
+    {
+        return quarkColor;
+    }
+
+    private void Awake()
+    {
+        sphereMaterial = quarkSphere.GetComponent<Renderer>().material;
+        coreMaterial = quarkCore.GetComponent<Renderer>().material;
+        glowMaterial = quarkGlow.GetComponent<Renderer>().material;
+        trailRenderer = GetComponent<TrailRenderer>();
+
+        changeOrbit = GetComponent<ChangeOrbit>();
+
+        UpdateColor();
+    }
+
+    private void Start()
+    {
+        trailRenderer.widthMultiplier = trailWidth;
+    }
+
+    private void Update()
+    {
+        float tNormalized = Mathf.Clamp01(changeOrbit.CurrentOrbitRadius / changeOrbit.MaxRadius);
+        float coeff = trailSizeCorrectionCurve.Evaluate(tNormalized);
+
+        trailRenderer.time = trailTime * coeff;
+    }
+
+    public void FlashToColor(QuarkColor toColor, Action onComplete = null)
+    {
+        Gradient g = new Gradient();
+        g.SetKeys(
+            new GradientColorKey[]
+            {
+                new GradientColorKey(QuarkSettings.GetColorRGB(quarkColor), 0),
+                new GradientColorKey(Color.white * 2f, 0.3f),
+                new GradientColorKey(QuarkSettings.GetColorRGB(toColor), 1)
+            },
+            new GradientAlphaKey[]
+            {
+                new GradientAlphaKey(QuarkSettings.GetColorRGB(quarkColor).a, 0),
+                new GradientAlphaKey(1, 0.3f),
+                new GradientAlphaKey(QuarkSettings.GetColorRGB(toColor).a, 1)
+            }
+        );
+
+        sphereMaterial.DOGradientColor(g, "_Color", QuarkSettings.Instance.GluonFlashTime).OnComplete(()=>
+        {
+            SetColor(toColor);
+            onComplete?.Invoke();
+        });
+
+        glowMaterial.DOGradientColor(g, "_Color", QuarkSettings.Instance.GluonFlashTime).OnComplete(()=>
+        {
+            SetColor(toColor);
+            onComplete?.Invoke();
+        });
+
+        trailRenderer.material.DOGradientColor(g, "_Color", QuarkSettings.Instance.GluonFlashTime).OnComplete(()=>
+        {
+            SetColor(toColor);
+            onComplete?.Invoke();
+        });
+
+    }
+}
